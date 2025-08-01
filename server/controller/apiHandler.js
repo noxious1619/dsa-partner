ccd6becf983403a3692ccf2384d5b4a6c19e2d4c
Dishant <shoonyam.108@gmail.com>
2025-08-01 08:19:05 +0530

    handled the custom prompt for different types of requests, initialied the frontend

--- server/controller/apiHandler.js
+++ server/controller/apiHandler.js
@@ -1,13 +1,26 @@
 import { main } from '../gemini.js';
 
+const generatePrompt = (type, prompt) => {
+  switch (type) {
+    case 'explanation':
+      return `Explain this DSA question in a beginner-friendly way:\n"${prompt}"`;
+    case 'hint':
+      return `Give a helpful hint to solve this DSA question:\n"${prompt}"`;
+    case 'answer':
+      return `Give a well-structured solution with code and explanation for this DSA question:\n"${prompt}"`; 
+  }
+};
+
 const askGemini = async (req, res) => {
-  const { prompt } = req.body;
+  const { prompt,type } = req.body;
   if (!prompt) {
     return res.status(400).json({ error: "Prompt is required" });
   }
 
+  const finalPrompt = generatePrompt(type, prompt);
+
   try {
-    const aiResponse = await main(prompt);
+    const aiResponse = await main(finalPrompt);
     res.status(200).json({ response: aiResponse });
   } catch (error) {
     console.error("Error from Gemini API:", error);
